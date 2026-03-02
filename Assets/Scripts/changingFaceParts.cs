fe15fd8f75f09df63addf214a20b451e9f0f756d
AsherMeow <aphroditeleebrown@gmail.com>
2026-03-02 16:55:41 +0000

    added a new eye type for testing purposes, and oh boy, got some code in, learning about ScriptableObjs etc

--- Assets/Scripts/changingFaceParts.cs
+++ Assets/Scripts/changingFaceParts.cs
@@ -1,48 +1,65 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public enum irisShape{
     defaultIris=1,
     star
 }
-public enum eyeShape{
+
+public enum eyeShapeType{
     defaultEye,
-    bunny
+    catty
 }
+
 public class changingFaceParts : MonoBehaviour
 {
-
-        public List<EyesSetting> test2= new List<EyesSetting>();
-
-
-    public irisShape IrisShape;
-
-    public void Change(irisShape change){
-        //Debug.Log("default");
-        switch ((int) change){
+    public eyeData eyeData;
+    public Texture[] getEyeTextures(eyeShapeType shape){
+        Texture[] textures= new Texture[2];
+        foreach(var setting in eyeData.eyeSettingsList){
+            if(setting.EyeShape == shape){
+                textures[0]= setting.LashTexture;
+                textures[1]= setting.ScleraTexture;
+                return textures;
+            }
+        }
+        return new Texture[2]{null,null};
+    }
+    public int rightEyeShape;
+    void setEyeTexture(eyeShapeType shape){
+        faceMaterial.SetTexture("_eyeLash",getEyeTextures(shape)[0]);
+        faceMaterial.SetTexture("_eyeSclera",getEyeTextures(shape)[1]);
+    }
+    public void changeRightEyeShape(int x){
+        switch(x){
             case 1:
-                Debug.Log("default");
+                setEyeTexture(eyeShapeType.defaultEye);
             break;
             case 2:
-                Debug.Log("star");
+                setEyeTexture(eyeShapeType.catty);
             break;
+            }
+    }
 
-            default:
-                Debug.Log("default");
+    string eROffset="_eyeROffset";
+    
+    public int rightEye;
+    public Material faceMaterial;
+    void Offset(string x,Vector2 y){
+        faceMaterial.SetVector(x,y);
+    }
+    public void changeRightEye(int x){
+        switch(x){
+            case 1:
+            Offset(eROffset,new Vector2(0.25f,0f));
+            break;
+            case 2:
+            Offset(eROffset,new Vector2(0.5f,0.75f));
             break;
         }
     }
     void Update(){
-        Change(IrisShape);
-        //public List<Sprite> test2= new List<Sprite>();
+        changeRightEye(rightEye);
+        changeRightEyeShape(rightEyeShape);
     }
 }
 
-[CreateAssetMenu(fileName = "EyesSetting", menuName = "Scriptable Objects/EyesSetting")]
-
-public class EyesSetting: ScriptableObject{
-    eyeShape EyeShape;
-    Sprite LashTexture;
-    Sprite ScleraTexture;
-
-}
--- Assets/Scripts/gulp.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "gulp", menuName = "Scriptable Objects/gulp")]
-public class gulp : ScriptableObject
-{
-    
-}
